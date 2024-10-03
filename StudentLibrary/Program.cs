2c4ce18bfcfbdb8971b4b33c84b7477f9c246d5d
James Connolly <jp.connolly@ulster.ac.uk>
2024-10-03 16:13:41 +0100

    Add project files.

---
+++ StudentLibrary/Program.cs
@@ -0,0 +1,22 @@
+﻿using StudentLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        Student student2 = new Student("Jane Doe", 18);
+
+        student1.Display();
+        student2.Display();
+
+        // Implement GetOlder
+        student1.GetOlder();
+        student2.GetOlder();
+
+        Console.WriteLine("Implementing student older");
+        student1.Display();
+        student2.Display();
+    }
+
+}
---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    class Student
+    {
+        private int id;
+        private string name;
+        private int age;
+        private static int studentCount = 0;
+
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static int StudentCount
+        {
+            get { return studentCount; }
+        }
+
+        // Default constructor
+        public Student()
+        {
+            name = "John Doe";
+            age = 16;
+            id = ++studentCount;
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            id = ++studentCount;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"StudentID: {id} Name: {name} Age: { age}");
+        }
+        public int GetOlder()
+        {
+            return ++age;
+        }
+
+    }
+}
